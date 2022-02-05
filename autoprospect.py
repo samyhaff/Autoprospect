3dc0fd7fae7a3f6c0bb5e7ef8d73c5f9b556beee
Samy Haffoudhi <haffoudhisamy@gmail.com>
2022-02-05 23:07:08 +0100

    message a la bdd sheets

--- autoprospect.py
+++ autoprospect.py
@@ -6,16 +6,16 @@ from sheets import get_mails
 EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
-msg = EmailMessage()
-msg['Subject'] = 'Test subject'
-msg['From'] =  EMAIL_ADDRESS
-msg['To'] = 'haffoudhisamy@gmail.com'
-msg.set_content('test content')
-
-adresses = get_mails()
-print(adresses)
+adresses = get_mails()[1:]
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
-    smtp.send_message(msg)
+    for adresse in adresses:
+        msg = EmailMessage()
+        msg['Subject'] = 'Message très important'
+        msg['From'] =  EMAIL_ADDRESS
+        msg['To'] = adresse
+        msg.set_content('Bonjour Messieurs Fidèles, le nom de mon père.....')
+
+        smtp.send_message(msg)
