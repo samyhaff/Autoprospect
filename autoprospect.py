7e5fb0af4ba2ccf9f3762cde8a803ad952012b98
Samy Haffoudhi <haffoudhisamy@gmail.com>
2022-02-06 15:41:07 +0100

    lecture d'arguments

--- autoprospect.py
+++ autoprospect.py
@@ -1,10 +1,21 @@
+#!/bin/python
+
 import os
+import argparse
 import smtplib
 from email.message import EmailMessage
 from sheets import get_mails
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+parser = argparse.ArgumentParser(description='Un script pour l\'envoi automatique de mails à une BDD')
+parser.add_argument("contenu", type=str, help='fichier contenant le mail à envoyer')
+parser.add_argument('-o', '--objet', type=str, help='object du mail', default='')
+args = parser.parse_args()
+
+contenu = args.contenu
+objet = args.objet
 
 adresses = get_mails()[1:]
 
@@ -16,7 +27,7 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 
     for adresse in adresses:
         msg = EmailMessage()
-        msg['Subject'] = 'Message très important'
+        msg['Subject'] = objet
         msg['From'] =  EMAIL_ADDRESS
         msg.set_content(contenu)
         msg['To'] = adresse
